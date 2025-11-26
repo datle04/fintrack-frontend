bc86c1d2ee8c8e3178c259e54ab8a2ca3c4cf123
Dat Le <ldat0909@gmail.com>
2025-11-26 17:46:00 +0700

    .

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,13 +4,24 @@ const BACK_END_URL = import.meta.env.VITE_BACK_END_URL;
 
 // Tạo một Axios instance
 const axiosInstance = axios.create({
-  // baseURL: BACK_END_URL,
-  baseURL: '/',
+  // baseURL: BACK_END_URL, trước
+  baseURL: '/', // sau
   headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json', // sau
   },
   withCredentials: true, // Quan trọng: Để gửi cookie tự động
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 
 export default axiosInstance; // Export instance đã cấu hình
