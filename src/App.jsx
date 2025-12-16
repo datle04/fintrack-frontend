d7b1b9aa0af4a8135b306467e20af488eab49d14
Dat Le <ldat0909@gmail.com>
2025-12-16 21:59:19 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,8 @@ function App() {
     }
   }, [i18n]);
 
+  getSocket().on("test_event", (data) => console.log("TEST OK:", data));
+
   return (
     <>
       {isAppLoading && <FullScreenLottie />}
