ebe005be2496fd318262e65539481a45181a7ab6
Dat Le <ldat0909@gmail.com>
2025-12-17 01:45:34 +0700

    .

--- src/features/transactionSlice.js
+++ src/features/transactionSlice.js
@@ -80,7 +80,7 @@ export const updateTransaction = createAsyncThunk(
   'transaction/updateTransaction',
   async ({ id, fields }, { rejectWithValue }) => { 
     try {
-      const res = await axiosInstance.put(
+      const res = await axiosInstance.patch(
         `/api/transaction/${id}`,
         fields, 
       );
